1589a1cc0ae788beefc5addae91789281e00b9d1
iawavgvst <alkelin1@gmail.com>
2024-11-08 02:08:42 +0300

    posts adding, modifications: front, redirect to start page (auth|register), sorting files into folders

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('post.index');
+        $posts = Post::all();
+        $likedPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(15);
+        return view('blog.post.index', compact('posts', 'likedPosts'));
     }
 }
